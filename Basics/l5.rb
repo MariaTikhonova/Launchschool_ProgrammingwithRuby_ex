d0e109e4fe601a18ee54d43489525bea99137bda
MariaTikhonova <ritz.rkraft@gmail.com>
2017-08-13 16:47:52 +0300

    Add files via upload

---
+++ Basics/l5.rb
@@ -0,0 +1,5 @@
+#5,6,7,8 fectorial
+p n1=(1..8).inject(1){|pr,n|pr*n}
+p n2=(1..7).inject(1){|pr,n|pr*n}
+p n2=(1..6).inject(1){|pr,n|pr*n}
+p n2=(1..5).inject(1){|pr,n|pr*n}
---
+++ Basics/l6.rb
@@ -0,0 +1,4 @@
+#SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+ # from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+ #This error tells us uncorect finishing of block or hash without curved brace
